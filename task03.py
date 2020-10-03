04f001adb55f9c7293628cb0828fb7533f7b4e7d
superhuman2020 <70416002+superhuman2020@users.noreply.github.com>
2020-10-03 12:34:52 -0700

    Answer
    Just my main program.

---
+++ task03.py
@@ -0,0 +1,18 @@
+def main():
+    calendar =  """Day               Time          Subject
+                    Monday   9:10 AM - 10:15 AM      Woodshop
+                              10:35 AM - 11:40 AM     Language Arts
+                              12:10 PM - 1:15 PM      Social Studies
+                    Tuesday  9:10 AM - 10:15 AM     PF
+                              10:35 AM - 11:40 AM  Science
+                              12:10 PM - 1:15 PM      Math"""
+   # This is my calendar.
+   if "Monday" in calendar:
+       print "Monday is there."
+   else:
+       print "Monday is not there."
+   # This is how Python will check if "Monday" is there in the schedule or not.
+   return#(without a function).
+   
+   print len(calendar)
+   # This is where I print my schedule's length using the len() function.
